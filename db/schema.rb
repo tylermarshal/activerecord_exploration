3c536c5857bcb8623c5aa24295fbf5e035632ad8
TMad <madsen.tyler@gmail.com>
2017-11-27 16:52:35 -0700

    add new stuff

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -14,11 +13,11 @@
 ActiveRecord::Schema.define(version: 20160818195543) do
 
   create_table "horses", force: :cascade do |t|
-    t.text     "name"
-    t.integer  "age"
-    t.integer  "total_winnings"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.text "name"
+    t.integer "age"
+    t.integer "total_winnings"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
